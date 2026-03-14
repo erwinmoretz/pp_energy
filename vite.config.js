ee6e22dbf7cbaf6a40e15816b8e964798af05389
erwin <erwin.moretz@wamocon.com>
2026-03-14 16:14:26 +0100

    aaa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/pp_energy/',
   plugins: [react(), tailwindcss()],
 })
